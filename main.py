026dbcb508d7ae104e7167cde4a08ecf7964bce8
SA44 <sa44@Skills-Academy-44.local>
2023-11-20 16:32:51 -0500

    Final Ver

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import random
 
-def play():
-    user = input("Choose your character! 'r' for rock, 'p' for paper, 's' for scissors\n")
-    computer = random.choice(['r', 'p', 's'])
-  
-    if user == computer: 
-       return 'It\'s a tie!? Time for another round!'
 
-    # r < p, p < s, s < r
-    if is_win(user, computer):
-        return 'You won!? Lucky shot, try again!'
+
+def play():
+  playagain = True
+  while playagain: 
     
-    return 'You lost. As expected... Stand up and fight again!'
+      user = input("Choose your character! 'r' for rock, 'p' for paper, 's' for scissors\n")
+      computer = random.choice(['r', 'p', 's'])
+    
+      if user == computer: 
+         print( 'It\'s a tie!? Time for another round!')
+  
+      # r < p, p < s, s < r
+      elif is_win(user, computer):
+        print('You won!? Lucky shot, try again!')
+      
+      else:   
+          print('You lost. As expected... Stand up and fight again!')
+    
+      
+      playagain = input("Play again? (y/n)\n") 
+      if playagain == 'n':
+         print('Thanks for playing!')
+         break
+    
 
 def is_win(player, opponent):
     # return True if player wins
@@ -21,3 +34,7 @@ def is_win(player, opponent):
         return True
 
 print(play())
+
+input_list = ["rock", "paper", "scissors"]
+
+print(input_list[1])
