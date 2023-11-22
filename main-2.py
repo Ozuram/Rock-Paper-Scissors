244b5281fad4ac357cd5a8d57b422b1333427f4a
SA44 <sa44@Skills-Academy-44.local>
2023-11-22 16:37:41 -0500

    python

--- main-2.py
+++ main-2.py
@@ -1,30 +1,39 @@
 import random
 
 
-
+#This function controls how the game works.
 def play():
   playagain = True
+  score = 0
   while playagain: 
-    
-      user = input("Choose your character! 'r' for rock, 'p' for paper, 's' for scissors\n")
-      computer = random.choice(['r', 'p', 's'])
-    
-      if user == computer: 
-         print( 'It\'s a tie!? Time for another round!')
-  
-      # r < p, p < s, s < r
-      elif is_win(user, computer):
-        print('You won!? Lucky shot, try again!')
+  #This is the list of options used in the game.
+      input_list = [" 'r' for rock", " 'p' for paper, ", "'s' for scissors"]
+    #This is the loop to print the list of options.
+      for i in input_list:
+        print(i)
+
+      user_input = input("Choose your character!\n")
       
+    
+      computer = random.choice(['r', 'p', 's'])
+
+      if user_input == computer: 
+         print( 'It\'s a tie!? Time for another round!')
+
+      # r < p, p < s, s < r
+      elif is_win(user_input, computer):
+        print('You won!? Lucky shot, try again!')
+        score += 1
+
       else:   
           print('You lost. As expected... Stand up and fight again!')
-    
-      
+
+
       playagain = input("Play again? (y/n)\n") 
       if playagain == 'n':
          print('Thanks for playing!')
          break
-    
+  print(f'You won number of times: {score}')
 
 def is_win(player, opponent):
     # return True if player wins
@@ -35,6 +44,3 @@ def is_win(player, opponent):
 
 print(play())
 
-input_list = ["rock", "paper", "scissors"]
-
-print(input_list[1])
